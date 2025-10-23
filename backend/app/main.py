83a16a651498b495cbba035fd0839bbf698ade8f
곽민지 <minji031@example.com>
2025-10-23 13:47:10 +0900

    cors추가

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,6 +33,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=r"http://localhost(:\d+)?",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
